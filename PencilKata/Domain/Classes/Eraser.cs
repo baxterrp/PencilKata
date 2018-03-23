efeb560b4e2c64214c16486d8471589ab7f27bcc
robert baxter <baxterrp@gmail.com>
2018-03-22 21:20:47 -0400

    Created Eraser class and new eraser unit tests

---
+++ PencilKata/Domain/Classes/Eraser.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Classes
+{
+    public class Eraser
+    {
+        /// <summary>
+        /// uses left of eraser
+        /// </summary>
+        private int Durability { get; set; }
+
+        public Eraser(int durability)
+        {
+            Durability = durability;
+        }
+
+        /// <summary>
+        /// Removes a word from the paper as entered
+        /// </summary>
+        /// <param name="word">the word to be removed</param>
+        /// <param name="paper">the paper written on</param>
+        /// <returns>the paper written on after erasing word</returns>
+        public Paper Erase(string word, Paper paper)
+        {
+            return paper; 
+        }
+    }
+}
---
+++ PencilKata/PencilKataUnitTests/EraserUnitTests.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Classes;
+using NUnit.Framework;
+
+namespace PencilKataUnitTests
+{
+
+    [TestFixture]
+    public class EraserUnitTests
+    {
+        private Eraser eraser;
+        private Paper paper;
+        private Pencil pencil;
+        private const string testSentence = "This is a test sentence";
+        private const string testSentenceWithWhiteSpace = "This is a      sentence";
+        private const int pencilDurability = 50;
+        private const int pencilLength = 10;
+        private const int eraserDurability = 5;
+
+        [SetUp]
+        public void SetupEraserTests()
+        {
+            pencil = new Pencil(pencilDurability, pencilLength);
+            paper = new Paper();
+            eraser = new Eraser(eraserDurability);
+        }
+
+        [Test]
+        public void TestEraserRemovesWordAsEntered()
+        {
+            // Arrange
+
+            // Act
+            paper = pencil.Write(testSentence, paper);
+            paper = eraser.Erase("test", paper);
+
+            // Assert
+            Assert.AreEqual(testSentenceWithWhiteSpace, paper.Text);
+        }
+
+        [Test]
+        public void TestEraserLosesDurabilityAfterEachUse()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public void TestEraserDoesNotEraseIfDurabilityIsZero()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
